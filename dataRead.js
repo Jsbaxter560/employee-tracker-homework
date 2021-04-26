4a8d197866368d587912e720b305a2e0df6c4db3
John Baxter <juancho420@ymail.com>
2021-04-26 00:01:56 -0700

    added prompt functions to give users options and gather input

--- dataRead.js
+++ dataRead.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql'); // establish dependency on mysql
 const inquirer = require('inquirer'); // establish dependency on inquirer
 const fs = require('fs'); // establish dependency on fs
+const { restoreDefaultPrompts } = require('inquirer');
 
 const connection = mysql.createConnection({ // create connectin 
     host: 'localhost', // establish local host
@@ -10,9 +11,43 @@ const connection = mysql.createConnection({ // create connectin
     database: 'trackerDB', // connect to database
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.table(departments);
-    connection.end();
-  });
+const start = () => { // prompt to ask user what they would like to do
+    inquirer
+        .prompt({
+            name: 'whatToDo',
+            type: 'list',
+            message: 'Would you like to do?',
+            choices: ['ADD', 'VIEW', 'UPDATE ROLE', 'EXIT'],
+        })
+        .then((answer) => {
+            if (answer.whatToDo === 'ADD') {
+                addPrompt();
+            } else if (answer.whatToDo === 'VIEW') {
+                viewPrompt();
+            } else if (answer.whatToDo === 'UPDATE ROLE') {
+                updatePrompt();
+            } else {
+                connection.end();
+            }
+        });
+};
+
+const addPrompt = () => { // prompt to ask user what they would like to add
+    inquirer
+        .prompt({
+            name: 'whatToAdd',
+            type: 'list',
+            message: 'What would you like to add?',
+            choices: ['DEPARTMENT', 'ROLE', 'EMPLOYEE'],
+        })
+        .then((answer) => {
+            if (answer.whatToAdd === 'DEPARTMENT') {
+                departmentPrompt();
+            } else if (answer.whatToAdd === 'ROLE') {
+                rolePrompt();
+            } else if (answer.whatToAdd === 'EMPLOYEE') {
+                employeePrompt();
+            }
+        });
+};
 
